d623466fea25eb3ec621bc8e7e83973d87d36b11
sdtm66 <2531966508@qq.com>
2021-06-10 09:04:10 +0800

    2

--- C++from0to1/P121_P126_operator_overload.cpp
+++ C++from0to1/P121_P126_operator_overload.cpp
@@ -24,21 +24,43 @@ public:
     int m_B;
 
 public:
-    Person operator+(Person &p)
+    //成员函数实现重载+号
+    Person operator+(Person &p) //加号运算符重载：赋予运算符新的定义，对自定义数据类型进行运算操作
     {
         Person temp;
         temp.m_A = this->m_A + p.m_A;
         temp.m_B = this->m_B + p.m_B;
         return temp;
     }
+    //运算符重载也可以发生函数重载
+    Person operator+(int a) //加号运算符重载：赋予运算符新的定义，对自定义数据类型进行运算操作
+    {
+        Person temp;
+        temp.m_A = this->m_A + a;
+        temp.m_B = this->m_B + a;
+        return temp;
+    }
 };
+
+//全局函数实现重载+号
+// Person operator+(Person &p1, Person &p2)
+// {
+//     Person temp;
+//     temp.m_A = p1.m_A + p2.m_A;
+//     temp.m_B = p1.m_B + p2.m_B;
+//     return temp;
+// }
 
 int main(void)
 {
     Person p1;
     Person p2;
-    Person p = p1 + p2;
+    Person p = p1 + p2; // Person +Person
+    //Person p = p1.operator+(p2); //成员函数实现重载+号本质调用
+    //Person p = operator+(p1,p2)//全局函数实现重载+号本质调用
     cout << p.m_A << ";" << p.m_B << endl;
-
+    //运算符重载也可以发生函数重载
+    Person p3 = p1 + 100; //Person+int
+    cout << p3.m_A << ";" << p3.m_B << endl;
     return 0;
 }
